d97911603d2dcf05fd45fa5a72e5056caaab0b9d
Aryan Thakur <52215513+importly@users.noreply.github.com>
2026-05-05 12:00:25 -0400

    feat: register sql/shell/deep-link plugins, emit window-blur/focus to frontend

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,26 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::Manager;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_sql::Builder::default().build())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let win = app.get_webview_window("main").unwrap();
+            let win_clone = win.clone();
+            win.on_window_event(move |event| {
+                if let tauri::WindowEvent::Focused(focused) = event {
+                    if *focused {
+                        win_clone.emit("window-focus", ()).ok();
+                    } else {
+                        win_clone.emit("window-blur", ()).ok();
+                    }
+                }
+            });
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
